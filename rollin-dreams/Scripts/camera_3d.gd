698bfcb3826a3f1dc3a87975802c6ec20cb588bd
griky <guidincruz@gmail.com>
2025-04-07 18:34:37 +0100

    initial gameplay

---
+++ rollin-dreams/Scripts/camera_3d.gd
@@ -0,0 +1,27 @@
+extends Camera3D
+
+@export var target: NodePath  # O jogador (bola)
+@export var follow_speed := 5.0  # A velocidade de seguimento da câmera
+@export var offset := Vector3(0, 3, 10)  # Distância da câmera em relação ao jogador
+
+var target_node: Node3D
+
+func _ready():
+	if target:
+		target_node = get_node(target)
+
+func _process(delta: float):
+	if target_node:
+		# Posição desejada da câmera com o offset
+		var desired_position = target_node.global_transform.origin + offset
+		
+		# Move a câmera suavemente em direção à posição desejada
+		global_transform.origin = global_transform.origin.lerp(desired_position, delta * follow_speed)
+		
+		# Faz a câmera olhar para o jogador
+		look_at(target_node.global_transform.origin, Vector3.UP)
+
+		# Rotaciona a câmera conforme a rotação do jogador
+		# Aqui estamos usando a rotação Y do jogador para ajustar a rotação da câmera
+		var player_rotation = target_node.rotation.y
+		rotation = Vector3(0, player_rotation, 0)
--- rollin-dreams/Scripts/player.gd
+++ rollin-dreams/Scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 7.5
 const JUMP_VELOCITY = 4.5
 
 
