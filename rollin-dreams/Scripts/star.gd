94725991d9dac3c3690dd115aedd3a54791a2066
DavidCastro21 <93283772+DavidCastro21@users.noreply.github.com>
2025-06-16 00:00:52 +0100

    some more sounds

--- rollin-dreams/Scripts/star.gd
+++ rollin-dreams/Scripts/star.gd
@@ -1,8 +1,10 @@
 extends Node3D
 @export var value: int =1
-
 func _on_area_3d_body_entered(body):
 	if body is Player:
 		GameController.candy_collected(value)
 		self.queue_free()
+		$CandySoundEmitter.play()
+
+
 	
